33e228c1340c164a21a57dc999ad62174e17b0d3
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-19 20:18:03 +0200

    Install supertest

--- test/users.test.js
+++ test/users.test.js
@@ -1,4 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
+// const request = require('supertest');
+const app = require('../src/app');
+
 describe('Users endpoints', () => {
   test('test', () => {
     expect(true).toBe(true);
