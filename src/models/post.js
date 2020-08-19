42a57657505aebb86c13514c69082b50988b43be
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-19 18:55:41 +0200

    Convert classes into mongoose schemas

--- src/models/post.js
+++ src/models/post.js
@@ -1,24 +1,33 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
 /**
  * Class to represent a post
  * @class Post
  */
-class Post {
-  /**
-   * Creates an instance of Post
-   * @param {string} title
-   * @param {string} photo
-   * @param {string} content
-   * @param {Array} animals
-   * @param {Array} tags
-   */
-  constructor(title, photo = '', content = '', animals = [], tags = []) {
-    this.title = title;
-    this.photo = photo;
-    this.content = content;
-    this.tags = tags;
-    this.animals = animals;
-    this.creationDate = new Date();
-  }
-}
+// class Post {
+//   /**
+//    * Creates an instance of Post
+//    * @param {string} title
+//    * @param {string} photo
+//    * @param {string} content
+//    * @param {Array} animals
+//    * @param {Array} tags
+//    */
+//   constructor(title, photo = '', content = '', animals = [], tags = []) {
+//     this.title = title;
+//     this.photo = photo;
+//     this.content = content;
+//     this.tags = tags;
+//     this.animals = animals;
+//     this.creationDate = new Date();
+//   }
+// }
 
-module.exports = Post;
+module.exports = mongoose.model('Post', postSchema);
