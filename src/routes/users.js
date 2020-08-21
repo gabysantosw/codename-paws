da7a3a9dbd566f9a9e6f172ab956f0192bd2eaa3
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-21 17:28:34 +0200

    Add tests for users.js route

--- src/routes/users.js
+++ src/routes/users.js
@@ -22,47 +22,50 @@ router.get('/init', async (req, res) => {
   res.sendStatus(200);
 });
 
-// GET all users & handle queries by name
+// GET all users & handle queries by name / city
 router.get('/', async (req, res) => {
   const query = {};
 
-  // check for a name query
+  // check query parameters by name or city
   if (req.query.name) {
     query.name = req.query.name;
   }
+  if (req.query.city) {
+    query.city = req.query.city;
+  }
 
-  // show all users since there's no query
-  // res.send(await Caretaker.find(query));
-  // await Caretaker.find(query);
-  const caretakers = await Caretaker.find(query);
+  const queryList = await Caretaker.find(query);
   // res.render('caretakers', { caretakers });
-  res.send(caretakers);
+  res.send(queryList);
+});
+
+// POST new caretaker to database
+router.post('/', async (req, res) => {
+  const newUser = await Caretaker.create(req.body);
+  res.send(newUser);
 });
 
 // GET users by id using query parameters
 router.get('/:userId', async (req, res) => {
   const user = await Caretaker.findById(req.params.userId);
-  // early return
-  // no user with that id -> 404 error
+  // no user was found with that id -> 404 error
   if (!user) return res.sendStatus(404);
 
-  console.log(user.info);
+  // console.log(user.info);
   // res.render('caretaker', { user });
   return res.json(user);
 });
 
+// POST new animal to a given caretaker
 router.post('/:userId/animals', async (req, res) => {
   const user = await Caretaker.findById(req.params.userId);
-  // early return
-  // no user with that id -> 404 error
+  // no user was found with that id -> 404 error
   if (!user) return res.sendStatus(404);
 
   const animal = await Animal.create({ name: req.body.name });
   await user.addAnimal(animal);
 
-  // res.render('caretaker', { user });
-  // res.json(user);
-  return res.send(user);
+  return res.send(animal);
 });
 
 module.exports = router;
