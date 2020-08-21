f475ba9351a896b43020617950a42125611720bc
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-21 11:38:29 +0200

    Add base route tests

---
+++ test/app.test.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+// const request = require('supertest');
+const supertest = require('supertest');
+const app = require('../src/app');
+
+describe('testtttt', () => {
+  test('test', () => {
+    expect(true).toBe(true);
+  });
+});
+
+// routes/users.js
+describe('Users endpoints', () => {
+  it('GET request to /users should list users', async () => {
+    // add user
+    const response = await supertest(app).get('/users');
+    expect(response.body.length > 0).toBe(true);
+  });
+
+  it('GET request to /users should list users', async () => {
+    // add user with Gaby name
+    const response = await supertest(app).get('/users?name=Gaby');
+    expect(response.body.length > 0).toBe(true);
+  });
+
+  it('GET request to /init should give an OK status', async () => {
+    const response = await supertest(app).get('/users/init');
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('POST request to /:userId/animals should add an animal', async () => {
+    // create user
+    // add animal
+    const response = await supertest(app).get('/users/init');
+
+    // check if added user has an animal with the same name
+    expect(response.statusCode).toBe(200);
+  });
+
+  // test /:userId
+  // create user
+  // get their id
+  // test existance
+});
