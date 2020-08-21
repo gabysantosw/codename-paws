8f54b80fc6e97b1971dbbc99dedf5ddfd9e4a839
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-21 11:39:04 +0200

    Create animals

---
+++ src/routes/animals.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+
+// const router = express.Router();
+
+// const Caretaker = require('../models/caretaker');
+// const Animal = require('../models/animal');
+// const Post = require('../models/post');
+
+// /animals?caretaker=gaby
+// /animals
+// /animals/:animalId
