09d49fa0a29771a833f723b95da5a1d0cb8e3d62
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-21 17:27:45 +0200

    Add tests for users.js route

--- test/app.test.js
+++ test/app.test.js
@@ -16,35 +16,52 @@ describe('Index', () => {
 
 // routes/users.js
 describe('Users endpoints', () => {
-  it('GET request to /users should list users', async () => {
-    // add user
+  const testUser = { name: 'Coyote', city: 'Pawnee' };
+  const testAnimal = { name: 'Lil' };
+  const testPost = { title: 'Hiii' };
+
+  it('POST request to /users should create a user', async () => {
+    const responseUser = await supertest(app).post('/users').send(testUser);
+    expect(responseUser.body.name).toBe(testUser.name);
+    expect(responseUser.body.city).toBe(testUser.city);
+  });
+
+  it('GET  request to /users should list at least one user', async () => {
+    await supertest(app).post('/users').send(testUser);
     const response = await supertest(app).get('/users');
     expect(response.body.length > 0).toBe(true);
   });
 
-  it('GET request to /users should list users', async () => {
-    // add user with Gaby name
-    const response = await supertest(app).get('/users?name=Gaby');
+  it('GET  request to /users with a name query should list at least one user', async () => {
+    await supertest(app).post('/users').send(testUser);
+    const response = await supertest(app).get(`/users?name=${testUser.name}`);
     expect(response.body.length > 0).toBe(true);
   });
 
-  it('GET request to /init should give an OK status', async () => {
-    const response = await supertest(app).get('/users/init');
+  it('GET  request to /users with a city query should list at least one user', async () => {
+    await supertest(app).post('/users').send(testUser);
+    const response = await supertest(app).get(`/users?city=${testUser.city}`);
+    expect(response.body.length > 0).toBe(true);
+  });
 
+  it('GET request to /users/init should give an OK status', async () => {
+    const response = await supertest(app).get('/users/init');
     expect(response.statusCode).toBe(200);
   });
 
-  it('POST request to /:userId/animals should add an animal', async () => {
-    // create user
-    // add animal
-    const response = await supertest(app).get('/users/init');
+  it('GET  request to /users/:userId should return an user', async () => {
+    const addedUser = (await supertest(app).post('/users').send(testUser)).body;
+    const response = await supertest(app).get(`/users/${addedUser._id}`);
 
-    // check if added user has an animal with the same name
-    expect(response.statusCode).toBe(200);
+    expect(response.body.name).toBe(testUser.name);
+    expect(response.body.city).toBe(testUser.city);
   });
 
-  // test /:userId
-  // create user
-  // get their id
-  // test existance
+  it('POST request to /users/:userId/animals should add an animal', async () => {
+    const addedUser = (await supertest(app).post('/users').send(testUser)).body;
+    // adding animal to user above
+    await supertest(app).post(`/users/${addedUser._id}/animals`).send(testAnimal);
+    const userWithAnimal = (await supertest(app).get(`/users/${addedUser._id}`)).body;
+    expect(userWithAnimal.animals.length === 1).toBe(true);
+  });
 });
