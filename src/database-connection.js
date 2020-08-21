0b687554f84d7b6f7ca66f6bf3e264f13dd310b1
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-21 17:29:06 +0200

    Remove debug mode

--- src/database-connection.js
+++ src/database-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGODB_CONNECTION_STRING;
 
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 
 mongoose
   .connect(connectionString, {
