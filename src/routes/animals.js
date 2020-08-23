3236cd95f22a4557beb5700d6df994e4be2cbd4a
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-23 20:08:04 +0200

    Add /animals route with GET request

--- src/routes/animals.js
+++ src/routes/animals.js
@@ -1,11 +1,29 @@
-// const express = require('express');
+const express = require('express');
 
-// const router = express.Router();
+const router = express.Router();
 
 // const Caretaker = require('../models/caretaker');
-// const Animal = require('../models/animal');
+const Animal = require('../models/animal');
 // const Post = require('../models/post');
 
 // /animals?caretaker=gaby
 // /animals
 // /animals/:animalId
+
+// <--===---===-->
+// <--===---===--> ANIMAL <--===---===--> //
+// <--===---===-->
+
+// GET all animals & handle queries by city
+router.get('/', async (req, res) => {
+  const query = {};
+
+  if (req.query.city) {
+    query.city = req.query.city;
+  }
+
+  const queryList = await Animal.find(query);
+  res.send(queryList);
+});
+
+module.exports = router;
