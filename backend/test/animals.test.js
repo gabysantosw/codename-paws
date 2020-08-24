644271817f54f1c4fdab2d6135356d95433ea170
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-24 21:12:45 +0200

    Create GET request tests for /animals route

---
+++ backend/test/animals.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+// const request = require('supertest');
+const supertest = require('supertest');
+const app = require('../src/app');
+
+// routes/index.js
+describe('Animals endpoints', () => {
+  const testUser = { name: 'Coyote', city: 'Pawnee' };
+  const testAnimal = { name: 'Lil' };
+  const testPost = { title: 'Hiii' };
+
+  it('GET  request to /animals should list at least one user', async () => {
+    const user = (await supertest(app).post('/api/users').send(testUser)).body;
+    await supertest(app).post(`/api/users/${user._id}/animals`).send(testAnimal);
+    const response = await supertest(app).get('/api/animals');
+    expect(response.body.length > 0).toBe(true);
+  });
+
+  it('GET  request to /animals with a city query should list at least one user', async () => {
+    const user = (await supertest(app).post('/api/users').send(testUser)).body;
+    const animal = (await supertest(app).post(`/api/users/${user._id}/animals`).send(testAnimal)).body;
+    const response = await supertest(app).get(`/api/animals?city=${animal.city}`);
+    expect(response.body.length > 0).toBe(true);
+  });
+});
