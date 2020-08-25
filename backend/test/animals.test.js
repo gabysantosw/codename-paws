2a401db6a14a76e84c3d40c1d5c0e1fa994ba68a
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-25 17:07:23 +0200

    Update Animal dummy object to also have a 'type' attribute

--- backend/test/animals.test.js
+++ backend/test/animals.test.js
@@ -9,17 +9,17 @@ const app = require('../src/app');
 // routes/index.js
 describe('Animals endpoints', () => {
   const testUser = { name: 'Coyote', city: 'Pawnee' };
-  const testAnimal = { name: 'Lil' };
-  const testPost = { title: 'Hiii' };
+  const testAnimal = { name: 'Lil', type: 'Dog' };
+  // const testPost = { title: 'Hiii' };
 
-  it('GET  request to /animals should list at least one user', async () => {
+  it('GET  request to /animals should list at least one animal', async () => {
     const user = (await supertest(app).post('/api/users').send(testUser)).body;
     await supertest(app).post(`/api/users/${user._id}/animals`).send(testAnimal);
     const response = await supertest(app).get('/api/animals');
     expect(response.body.length > 0).toBe(true);
   });
 
-  it('GET  request to /animals with a city query should list at least one user', async () => {
+  it('GET  request to /animals with a city query should list at least one animal', async () => {
     const user = (await supertest(app).post('/api/users').send(testUser)).body;
     const animal = (await supertest(app).post(`/api/users/${user._id}/animals`).send(testAnimal)).body;
     const response = await supertest(app).get(`/api/animals?city=${animal.city}`);
