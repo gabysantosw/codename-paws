45828aa6bfe05e76e755f275eb3f55d7045185b6
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-25 17:16:31 +0200

    Remove the 'profile' method to output info into the console, better formatting

--- backend/src/models/caretaker.js
+++ backend/src/models/caretaker.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const autopopulate = require('mongoose-autopopulate');
 
-const Cfonts = require('cfonts');
-require('colors');
-
 const caretakerSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -38,31 +35,6 @@ const caretakerSchema = new mongoose.Schema({
  */
 class Caretaker {
   /**
-   * Creates an instance of Caretaker
-   * @param {string} name
-   * @param {string} email
-   * @param {string} city
-   */
-  /*
-  constructor(name, email = '', city = '') {
-    this.name = name;
-    this.city = city;
-    this.email = email;
-
-    this.creationDate = new Date();
-    this.photo = '';
-    this.description = '';
-    this.links = [];
-    this.socials = [];
-    this.acceptsVolunteers = true;
-    this.acceptsDonations = true;
-
-    this.animals = [];
-    this.posts = [];
-  }
-  */
-
-  /**
    * Add a new animal to animals list
    * @param {Object} animal
    */
@@ -78,21 +50,6 @@ class Caretaker {
   async addPost(post) {
     this.posts.push(post);
     await this.save();
-  }
-
-  get info() {
-    Cfonts.say('  - Dashboard -  ', {
-      font: 'tiny',
-      colors: ['cyan'],
-      letterSpacing: 2,
-      background: 'black',
-    });
-    return `
-${'Name'.rainbow.bold}: ${this.name.bold.cyan}
-${'Animals in care'.rainbow.bold}: ${this.animals.map(animal => `${animal.name}`).join(', ').bold.cyan}
-${'Posts'.rainbow.bold} (${String(this.posts.length).bold.cyan}):
-${this.posts.map(post => `${'- '.bold.cyan}${post.title}`).join('\n')}
-        `;
   }
 }
 
