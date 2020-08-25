36e4480c9eba522bee153cbe8c228b0eccefc3a1
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-25 17:21:23 +0200

    Change filenames of views and App to be case-insensitive

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,11 +4,13 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     ul 
       li(v-for='user in users') {{ user.name }}
+        ul
+          li(v-for='animal in user.animals') {{animal.name}} {{animal.city}} {{animal.type}}
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/hello-world.vue';
 import axios from 'axios';
 
 export default {
@@ -22,6 +24,7 @@ export default {
   async created() {
     const request = await axios.get('/api/users');
     this.users = request.data;
+    console.log(this.users);
   }
 };
 </script>
