135fe26577f07efa4a4b3116ec2416a37bb76003
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-26 20:21:40 +0200

    Add ShelterAnimal component, improve template

--- frontend/src/components/shelter-card.vue
+++ frontend/src/components/shelter-card.vue
@@ -1,12 +1,20 @@
-<template lang="pug">
-  li {{ user.name }}, location: {{ user.city }}. Animals in care: {{ user.animals.length }}
-</template>
-
 <script>
+import ShelterAnimal from '@/components/shelter-animal.vue';
+
 export default {
   name: 'ShelterCard',
-  props: ['user']
+  props: ['user'],
+  components: { ShelterAnimal }
 };
 </script>
+
+<template lang="pug">
+  li 
+    h2 {{ user.name }}
+    p Location: {{ user.city }}. 
+    h3(v-if='user.animals.length > 0') Animals in care ({{ user.animals.length }})
+    ul(v-if='user.animals.length > 0')
+      ShelterAnimal(v-for='animal in user.animals' :animal='animal')
+</template>
 
 <style lang="scss"></style>
