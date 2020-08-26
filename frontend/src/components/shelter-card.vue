638b2ecea1a0b4f4d784997dab95ffc28bb17db1
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-26 19:13:21 +0200

    Create base component to visualize shelters

---
+++ frontend/src/components/shelter-card.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  li {{ user.name }}, location: {{ user.city }}. Animals in care: {{ user.animals.length }}
+</template>
+
+<script>
+export default {
+  name: 'ShelterCard',
+  props: ['user']
+};
+</script>
+
+<style lang="scss"></style>
