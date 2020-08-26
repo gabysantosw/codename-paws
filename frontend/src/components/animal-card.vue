7aa19680975df473dce6d3103bffc6f5c9fa8115
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-26 19:14:34 +0200

    Create component to handle the display of each animal individually

---
+++ frontend/src/components/animal-card.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  li {{ animal.name }}, location: {{ animal.city }}
+</template>
+
+<script>
+export default {
+  name: 'AnimalCard',
+  props: ['animal']
+};
+</script>
+
+<style lang="scss"></style>
