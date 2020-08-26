ea56195d6086605d3dfbbf7beec0f1b23b0a04d2
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-26 19:15:26 +0200

    Remove the 'about' view to include animals

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">App</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Shelters</router-link> |
+      <router-link to="/animals">Animals</router-link>
     </div>
     <router-view />
   </div>
