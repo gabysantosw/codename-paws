4df98c2a12a1fb769e51e18c171b087e2c4fcbe4
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-26 19:14:08 +0200

    Create animals view to display all the adoptable animals

---
+++ frontend/src/views/animals.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  section
+    h1 All animals looking to be adopted
+    ul(v-if='animals.length > 0')
+      AnimalCard(v-for='animal in animals' :animal='animal')
+    p(v-else) No animals
+</template>
+
+<script>
+import AnimalCard from '@/components/animal-card.vue';
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: { AnimalCard },
+  data() {
+    return {
+      animals: []
+    };
+  },
+  async created() {
+    const request = await axios.get('/api/animals');
+    this.animals = request.data;
+  }
+};
+</script>
