2779a7a9cbba7664d64825145d865017f2f7f5bb
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-27 21:53:42 +0200

    Create fetchUsers and fetchAnimals actions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async fetchUsers() {
+      const request = await axios.get('/api/users');
+      return request.data;
+    },
+    async fetchAnimals() {
+      const request = await axios.get('/api/animals');
+      return request.data;
+    }
+  },
   modules: {}
 });
