d4c3dea99d5d103c8c4ea934162432feb05097d6
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-28 20:36:19 +0200

    Add detailed pages for users and animals

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,6 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/animals.vue')
+  },
+  {
+    path: '/users/:id',
+    name: 'DetailedShelter',
+    component: () => import(/* webpackChunkName: "about" */ '../views/detailed-shelter.vue')
+  },
+  {
+    path: '/animals/:id',
+    name: 'DetailedAnimal',
+    component: () => import(/* webpackChunkName: "about" */ '../views/detailed-animal.vue')
   }
 ];
 
