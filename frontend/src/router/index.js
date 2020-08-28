894646954556db900452645515883683d9e26ff4
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-08-28 20:36:48 +0200

    Change name of 'home' view for 'shelters'

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/home.vue';
+import Shelters from '../views/shelters.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Shelters',
+    component: Shelters
   },
   {
     path: '/animals',
--- frontend/src/views/shelters.vue
+++ frontend/src/views/shelters.vue
@@ -1,18 +1,9 @@
-<template lang="pug">
-  .home
-    h1 All shelters, associations or individuals
-    ul(v-if='users.length > 0')
-      ShelterCard(v-for='user in users' :user='user')
-    p(v-else) No shelters
-</template>
-
 <script>
-// @ is an alias to /src
 import ShelterCard from '@/components/shelter-card.vue';
 import { mapActions } from 'vuex';
 
 export default {
-  name: 'Home',
+  name: 'Shelters',
   components: { ShelterCard },
   data() {
     return {
@@ -27,3 +18,11 @@ export default {
   }
 };
 </script>
+
+<template lang="pug">
+  .home
+    h1 All shelters, associations or individuals
+    ul(v-if='users.length > 0')
+      ShelterCard(v-for='user in users' :user='user')
+    p(v-else) No shelters
+</template>
