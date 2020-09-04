d49e94ee0284c22b22c4a12f8c8b569da6cb829c
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-04 13:13:03 +0200

    Change naming of test object and comment out the use of mongodb-memory-server

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -2,9 +2,50 @@
 /* eslint-disable no-console */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
-// const request = require('supertest');
+// const mongoose = require('mongoose');
+// const { MongoMemoryServer } = require('mongodb-memory-server');
+
 const supertest = require('supertest');
 const app = require('../src/app');
+
+// const CaretakerModel = require('../src/models/caretaker');
+// const AnimalModel = require('../src/models/animal');
+// const PostModel = require('../src/models/post');
+
+// // Start MongoDB instance
+// const mongod = new MongoMemoryServer();
+
+// // start the database and populate records before testing
+// beforeAll(async () => {
+//   const uri = await mongod.getConnectionString();
+//   const debug = false;
+//   await mongoose
+//     .connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     })
+//     .then(() => {
+//       if (debug) {
+//         console.log('Fake Mongo connected');
+//       }
+//     })
+//     .catch(err => console.error(err.message));
+
+//   // populating database with dummy data
+//   const testCaretaker = new CaretakerModel({ name: 'Coyote', city: 'Pawnee' });
+//   const testAnimal = new AnimalModel({ name: 'Lil', type: 'Dog' });
+//   const testPost = new PostModel({ title: 'Hiii' });
+
+//   await testCaretaker.save();
+//   await testAnimal.save();
+//   await testPost.save();
+// });
+
+// afterAll(async () => {
+//   await mongoose.disconnect();
+//   await mongod.stop();
+// });
 
 // routes/index.js
 describe('Index', () => {
@@ -16,31 +57,31 @@ describe('Index', () => {
 
 // routes/users.js
 describe('Users endpoints', () => {
-  const testUser = { name: 'Coyote', city: 'Pawnee' };
+  const testCaretaker = { name: 'Coyote', city: 'Pawnee' };
   const testAnimal = { name: 'Lil', type: 'Dog' };
   const testPost = { title: 'Hiii' };
 
   it('POST request to /users should create a user', async () => {
-    const responseUser = await supertest(app).post('/api/users').send(testUser);
-    expect(responseUser.body.name).toBe(testUser.name);
-    expect(responseUser.body.city).toBe(testUser.city);
+    const responseUser = await supertest(app).post('/api/users').send(testCaretaker);
+    expect(responseUser.body.name).toBe(testCaretaker.name);
+    expect(responseUser.body.city).toBe(testCaretaker.city);
   });
 
   it('GET  request to /users should list at least one user', async () => {
-    await supertest(app).post('/api/users').send(testUser);
+    await supertest(app).post('/api/users').send(testCaretaker);
     const response = await supertest(app).get('/api/users');
     expect(response.body.length > 0).toBe(true);
   });
 
   it('GET  request to /users with a name query should list at least one user', async () => {
-    await supertest(app).post('/api/users').send(testUser);
-    const response = await supertest(app).get(`/api/users?name=${testUser.name}`);
+    await supertest(app).post('/api/users').send(testCaretaker);
+    const response = await supertest(app).get(`/api/users?name=${testCaretaker.name}`);
     expect(response.body.length > 0).toBe(true);
   });
 
   it('GET  request to /users with a city query should list at least one user', async () => {
-    await supertest(app).post('/api/users').send(testUser);
-    const response = await supertest(app).get(`/api/users?city=${testUser.city}`);
+    await supertest(app).post('/api/users').send(testCaretaker);
+    const response = await supertest(app).get(`/api/users?city=${testCaretaker.city}`);
     expect(response.body.length > 0).toBe(true);
   });
 
@@ -50,11 +91,11 @@ describe('Users endpoints', () => {
   });
 
   it('GET  request to /users/:userId should return an user', async () => {
-    const addedUser = (await supertest(app).post('/api/users').send(testUser)).body;
+    const addedUser = (await supertest(app).post('/api/users').send(testCaretaker)).body;
     const response = await supertest(app).get(`/api/users/${addedUser._id}`);
 
-    expect(response.body.name).toBe(testUser.name);
-    expect(response.body.city).toBe(testUser.city);
+    expect(response.body.name).toBe(testCaretaker.name);
+    expect(response.body.city).toBe(testCaretaker.city);
   });
 
   it('GET  request to /users/:userId with a non-existent user should return 404', async () => {
@@ -77,7 +118,7 @@ describe('Users endpoints', () => {
   // <--===---===--> ANIMAL <--===---===--> //
 
   it('GET  request to /users/:userId/animals should list at least one animal', async () => {
-    const user = (await supertest(app).post('/api/users').send(testUser)).body;
+    const user = (await supertest(app).post('/api/users').send(testCaretaker)).body;
     await supertest(app).post(`/api/users/${user._id}/animals`).send(testAnimal);
     const response = await supertest(app).get(`/api/users/${user._id}/animals`);
     expect(response.body.length).toBe(1);
@@ -86,7 +127,7 @@ describe('Users endpoints', () => {
   it('POST  request to /users/:userId/animals with an specific city, should match the given city', async () => {
     const testAnimalWithCity = { name: 'Choco', city: 'Caracas', type: 'Dog' };
 
-    const user = (await supertest(app).post('/api/users').send(testUser)).body;
+    const user = (await supertest(app).post('/api/users').send(testCaretaker)).body;
     await supertest(app).post(`/api/users/${user._id}/animals`).send(testAnimalWithCity);
     const response = await supertest(app).get(`/api/users/${user._id}/animals`);
     expect(response.body[0].city).toBe(testAnimalWithCity.city);
@@ -98,7 +139,7 @@ describe('Users endpoints', () => {
   });
 
   it('POST request to /users/:userId/animals should add an animal', async () => {
-    const addedUser = (await supertest(app).post('/api/users').send(testUser)).body;
+    const addedUser = (await supertest(app).post('/api/users').send(testCaretaker)).body;
     // adding animal to user above
     await supertest(app).post(`/api/users/${addedUser._id}/animals`).send(testAnimal);
 
@@ -114,7 +155,7 @@ describe('Users endpoints', () => {
   // <--===---===--> POST <--===---===--> //
 
   it('GET  request to /users/:userId/posts should list at least one post', async () => {
-    const user = (await supertest(app).post('/api/users').send(testUser)).body;
+    const user = (await supertest(app).post('/api/users').send(testCaretaker)).body;
     await supertest(app).post(`/api/users/${user._id}/posts`).send(testPost);
     const response = await supertest(app).get(`/api/users/${user._id}/posts`);
     expect(response.body.length).toBe(1);
@@ -126,7 +167,7 @@ describe('Users endpoints', () => {
   });
 
   it('POST request to /users/:userId/posts should add a post', async () => {
-    const addedUser = (await supertest(app).post('/api/users').send(testUser)).body;
+    const addedUser = (await supertest(app).post('/api/users').send(testCaretaker)).body;
     // adding post to user above
     await supertest(app).post(`/api/users/${addedUser._id}/posts`).send(testPost);
 
