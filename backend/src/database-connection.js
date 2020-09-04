ba4be0810783222fed7f6c7ca169d66d27a1884d
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-04 13:16:20 +0200

    Set an empty string as default for the MongoDB string

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_CONNECTION_STRING;
+const connectionString = process.env.MONGODB_CONNECTION_STRING || '';
 
 // mongoose.set('debug', true);
 
