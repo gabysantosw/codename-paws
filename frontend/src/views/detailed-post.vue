d4bf5d05a9ac1c6fa382075af9a51c364991648f
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-11 20:26:56 +0200

    Create DetailedPost to view the complete post in one page

---
+++ frontend/src/views/detailed-post.vue
@@ -0,0 +1,24 @@
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'DetailedAnimal',
+  data() {
+    return {
+      post: null
+    };
+  },
+  async mounted() {
+    this.post = await this.fetchPostById(this.$route.params.id);
+  },
+  methods: {
+    ...mapActions(['fetchPostById'])
+  }
+};
+</script>
+
+<template lang="pug">
+  li(v-if='post') 
+    h2 {{ post.title }}
+    p this is a post
+</template>
