8572b2b62c3152adec258b5d07845572e5f112d3
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-11 14:52:12 +0200

    Add login and register routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,44 +1,61 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/home.vue';
+import Login from '../views/login.vue';
+import Register from '../views/register.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/shelters',
-    name: 'Shelters',
-    component: () => import(/* webpackChunkName: "about" */ '../views/shelters.vue')
-  },
-  {
-    path: '/animals',
-    name: 'Animals',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/animals.vue')
-  },
-  {
-    path: '/shelters/:id',
-    name: 'DetailedShelter',
-    component: () => import(/* webpackChunkName: "about" */ '../views/detailed-shelter.vue')
-  },
-  {
-    path: '/animals/:id',
-    name: 'DetailedAnimal',
-    component: () => import(/* webpackChunkName: "about" */ '../views/detailed-animal.vue')
-  }
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-});
-
-export default router;
+export default store => {
+  return new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/shelters',
+        name: 'Shelters',
+        component: () => import(/* webpackChunkName: "about" */ '../views/shelters.vue')
+      },
+      {
+        path: '/animals',
+        name: 'Animals',
+        props: true,
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/animals.vue')
+      },
+      {
+        path: '/shelters/:id',
+        name: 'DetailedShelter',
+        component: () => import(/* webpackChunkName: "about" */ '../views/detailed-shelter.vue')
+      },
+      {
+        path: '/animals/:id',
+        name: 'DetailedAnimal',
+        component: () => import(/* webpackChunkName: "about" */ '../views/detailed-animal.vue')
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: Register,
+        beforeEnter(to, from, next) {
+          if (store.state.account) return next('/');
+          return next();
+        }
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        beforeEnter(to, from, next) {
+          if (store.state.account) return next('/');
+          return next();
+        }
+      }
+    ]
+  });
+};
