6f6b698e618af0cd2d2b2b1677d8cde7692f9c2a
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-11 14:49:44 +0200

    Add select to filter shelters by city

--- frontend/src/views/shelters.vue
+++ frontend/src/views/shelters.vue
@@ -7,14 +7,36 @@ export default {
   components: { ShelterCard },
   data() {
     return {
-      shelters: []
+      shelters: [],
+      cityList: [],
+      selectedCity: 'All'
     };
   },
   async created() {
     this.shelters = await this.fetchShelters();
+    this.cityList = this.shelters.reduce(
+      (list, currentShelter) => {
+        if (!list || !list.includes(currentShelter.city)) {
+          list.push(currentShelter.city);
+        }
+        return list;
+      },
+      ['All']
+    );
   },
   methods: {
-    ...mapActions(['fetchShelters'])
+    ...mapActions(['fetchShelters', 'fetchSheltersBy']),
+    async filterShelters() {
+      this.shelters = await this.fetchSheltersBy(this.query);
+    }
+  },
+  computed: {
+    query() {
+      const params = {};
+      if (this.selectedCity !== 'All') params.city = this.selectedCity;
+      if (this.selectedType !== 'All') params.type = this.selectedType;
+      return params;
+    }
   }
 };
 </script>
@@ -22,6 +44,10 @@ export default {
 <template lang="pug">
   .home
     h1 Shelters
+    section
+      label(for='city-select') Cities:
+      select(name='city' id='city-select' v-model='selectedCity' @change='filterShelters()')
+        option(v-for='city in cityList' :value='city') {{ city }}
     ul(v-if='shelters.length > 0')
       ShelterCard(v-for='shelter in shelters' :shelter='shelter')
     p(v-else) No shelters
