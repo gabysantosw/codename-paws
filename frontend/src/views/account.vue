dbc8ddf094dafe144670c668bbd7096e05c0efd8
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-14 18:14:51 +0200

    Hide the 'view all' links if there's no animals / posts respectively

--- frontend/src/views/account.vue
+++ frontend/src/views/account.vue
@@ -26,12 +26,12 @@ export default {
     p {{ shelter.name }}
     h2 Animals in care ({{ shelter.animals.length }})
     router-link(to='/account/add-animal') Add animal
-    router-link(to='/account/view-animals') View all
+    router-link(v-if='shelter.animals.length' to='/account/view-animals') View all
     ul 
       li(v-for='animal of shelter.animals') {{ animal.name }}
     h2 Posts ({{ shelter.posts.length }})
     router-link(to='/account/add-post') Create new post
-    router-link(to='/account/view-posts') View all
+    router-link(v-if='shelter.posts.length' to='/account/view-posts') View all
     ul 
       li(v-for='post of shelter.posts') {{ post.title }}
 </template>
