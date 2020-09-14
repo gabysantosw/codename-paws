522797559cb1a7858c76eaf305e7f27ac92c27be
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-14 17:59:00 +0200

    Create view animals page to show all shelter's animals, allows for deletion

---
+++ frontend/src/views/view-animals.vue
@@ -0,0 +1,34 @@
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'ViewAnimals',
+  data() {
+    return {
+      animals: []
+    };
+  },
+  async created() {
+    this.animals = await this.fetchShelterAnimals(this.account._id);
+  },
+  methods: {
+    ...mapActions(['fetchShelterAnimals', 'deleteAnimalById']),
+    async deleteAnimal(id) {
+      await this.deleteAnimalById(id);
+      this.animals = await this.fetchShelterAnimals(this.account._id);
+    }
+  },
+  computed: {
+    ...mapState(['account'])
+  }
+};
+</script>
+
+<template lang="pug">
+  main
+    h1 All animals
+    ul 
+      li(v-for='animal of animals') 
+        span {{ animal.name }}
+        button(@click='deleteAnimal(animal._id)') Remove
+</template>
