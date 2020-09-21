d78bc6751a842dafdbf813748644df197cdffbfc
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-21 17:56:24 +0200

    Create action for PUT request to update Post by ID

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -71,6 +71,10 @@ const store = new Vuex.Store({
       const request = await axios.post(`/api/shelters/${post.shelterId}/posts`, post);
       return request.data;
     },
+    async updatePostById(store, { postId, post }) {
+      const request = await axios.put(`/api/posts/${postId}`, post);
+      return request.data;
+    },
     async deletePostById(store, postId) {
       const request = await axios.delete(`/api/posts/${postId}`);
       return request.data;
