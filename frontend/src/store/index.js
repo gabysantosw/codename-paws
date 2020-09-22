169c369572381e26c1e3902277996850b501d74e
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-22 18:05:05 +0200

    Modularize actions referent to shelters / animals / posts for a more readable store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,10 @@ import axios from 'axios';
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 axios.defaults.withCredentials = true;
 
+import shelters from './shelters.js';
+import animals from './animals.js';
+import posts from './posts.js';
+
 Vue.use(Vuex);
 
 const SET_ACCOUNT = 'set account';
@@ -19,70 +23,6 @@ const store = new Vuex.Store({
     },
   },
   actions: {
-    async fetchShelters() {
-      const request = await axios.get('/api/shelters');
-      return request.data;
-    },
-    async fetchSheltersBy(store, query) {
-      const request = await axios.get(`/api/shelters`, { params: query });
-      return request.data;
-    },
-    async fetchShelterById(store, shelterId) {
-      const request = await axios.get(`/api/shelters/${shelterId}`);
-      return request.data;
-    },
-    async fetchShelterAnimals(store, shelterId) {
-      const request = await axios.get(`/api/shelters/${shelterId}/animals`);
-      return request.data;
-    },
-    async fetchShelterPosts(store, shelterId) {
-      const request = await axios.get(`/api/shelters/${shelterId}/posts`);
-      return request.data;
-    },
-    async updateShelterById(store, { shelterId, shelter }) {
-      const request = await axios.put(`/api/shelters/${shelterId}`, shelter);
-      return request.data;
-    },
-    async fetchAnimals() {
-      const request = await axios.get('/api/animals');
-      return request.data;
-    },
-    async fetchAnimalsBy(store, query) {
-      const request = await axios.get(`/api/animals`, { params: query });
-      return request.data;
-    },
-    async fetchAnimalById(store, animalId) {
-      const request = await axios.get(`/api/animals/${animalId}`);
-      return request.data;
-    },
-    async postAnimal(store, animal) {
-      const request = await axios.post(`/api/shelters/${animal.shelterId}/animals`, animal);
-      return request.data;
-    },
-    async updateAnimalById(store, { animalId, animal }) {
-      const request = await axios.put(`/api/animals/${animalId}`, animal);
-      return request.data;
-    },
-    async deleteAnimalById(store, animalId) {
-      const request = await axios.delete(`/api/animals/${animalId}`);
-      return request.data;
-    },
-    async fetchPostById(store, postId) {
-      const request = await axios.get(`/api/posts/${postId}`);
-      return request.data;
-    },
-    async postPost(store, post) {
-      const request = await axios.post(`/api/shelters/${post.shelterId}/posts`, post);
-      return request.data;
-    },
-    async updatePostById(store, { postId, post }) {
-      const request = await axios.put(`/api/posts/${postId}`, post);
-      return request.data;
-    },
-    async deletePostById(store, postId) {
-      const request = await axios.delete(`/api/posts/${postId}`);
-      return request.data;
-    },
     // AUTHENTICATION
     async fetchSession({ commit }) {
       const account = await axios.get('/api/account/session');
@@ -100,7 +40,11 @@ const store = new Vuex.Store({
       commit(SET_ACCOUNT, null);
     },
   },
-  modules: {},
+  modules: {
+    shelters,
+    animals,
+    posts,
+  },
 });
 
 export default async () => {
