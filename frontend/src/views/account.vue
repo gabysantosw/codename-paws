314e9ea70edbf0cb4012c94cb05ac819a413f936
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-24 19:12:27 +0200

    Use the confirmation-modal component instead of deleting an account directly

--- frontend/src/views/account.vue
+++ frontend/src/views/account.vue
@@ -1,22 +1,29 @@
 <script>
 import { mapState, mapActions } from 'vuex';
+import ConfirmationModal from '@/components/confirmation-modal.vue';
 
 export default {
   name: 'Account',
+  components: {
+    ConfirmationModal,
+  },
   async created() {
     this.shelter = await this.fetchShelterById(this.account._id);
   },
   data() {
     return {
       shelter: null,
+      isModalOpen: false,
     };
   },
   methods: {
-    ...mapActions(['fetchShelterById', 'deleteShelterById', 'logout']),
-    async deleteAccount() {
-      await this.deleteShelterById(this.account._id);
-      await this.logout();
-      this.$router.push('/');
+    ...mapActions(['fetchShelterById']),
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+      this.$nextTick(() => this.$refs.openButton.focus());
     },
   },
   computed: {
@@ -30,7 +37,7 @@ export default {
     h1 Account information
     p {{ shelter.name }} - {{ shelter.city }}
     router-link(to='/account/edit-shelter') Edit information
-    button(@click='deleteAccount') Delete account
+    button(@click='openModal' ref='openButton') Delete account
     h2 Animals in care ({{ shelter.animals.length }})
     router-link(to='/account/add-animal') Add animal
     router-link(v-if='shelter.animals.length' to='/account/view-animals') View all
@@ -41,4 +48,5 @@ export default {
     router-link(v-if='shelter.posts.length' to='/account/view-posts') View all
     ul 
       li(v-for='post of shelter.posts') {{ post.title }}
+    ConfirmationModal(:shelterId='account._id' :isModalOpen='isModalOpen' @close='closeModal()')
 </template>
