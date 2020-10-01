73078a69814a9c1f880cf66971cc0636d8b11ff9
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-10-01 19:52:08 +0200

    Handle upload / deletion of image when an animal gets updated / deleted

--- backend/src/routes/animals.js
+++ backend/src/routes/animals.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { uploadImage, deleteImage } = require('../helpers/cloud-storage');
 
 const router = express.Router();
 
+const Shelter = require('../models/shelter');
 const Animal = require('../models/animal');
 
 // <--===---===-->
@@ -39,17 +41,47 @@ router.delete('/:animalId', async (req, res) => {
   // so if there's none, no animal with that Id was found -> 404
   if (!animal) return res.sendStatus(404);
 
+  // remove associated image from cloud storage
+  if (animal.image) {
+    await deleteImage(animal.image);
+  }
+
   // removed successfully
   return res.sendStatus(200);
 });
 
 // PUT / update animal by ID
 router.put('/:animalId', async (req, res) => {
-  const animal = await Animal.findOneAndUpdate({ _id: req.params.animalId }, req.body);
+  const shelter = await Shelter.findById(req.body.shelterId);
 
-  if (!animal) return res.sendStatus(404);
+  // no shelter was found with that id -> 404 error
+  if (!shelter) return res.sendStatus(404);
 
-  // removed successfully
+  if (shelter.animals.includes(req.params.animalId)) return res.sendStatus(404);
+
+  const animal = await Animal.findById(req.params.animalId);
+
+  const { name, city, type } = req.body;
+
+  // if an image was given, upload to cloud storage
+  // when no file was added in the form req.file = undefined
+  let imageUrl;
+  if (req.file) {
+    // check if animal already had an image, delete before uploading new
+    if (animal.image) {
+      await deleteImage(animal.image);
+    }
+
+    const { originalname, buffer } = req.file;
+
+    imageUrl = await uploadImage(buffer, originalname, `${shelter.name}-${name}-${shelter.animals.length}`);
+
+    await Animal.findOneAndUpdate({ _id: req.params.animalId }, { name, city, type, image: imageUrl });
+  } else {
+    await Animal.findOneAndUpdate({ _id: req.params.animalId }, { name, city, type });
+  }
+
+  // updated successfully
   return res.sendStatus(200);
 });
 
